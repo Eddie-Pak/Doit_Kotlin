f55188c712b46cb613be115abdf4068c936e622c
Eddie-Pak <foalstn@naver.com>
2024-03-08 17:57:08 +0900

    first commit

---
+++ src/chap02/section2/ValVar.kt
@@ -0,0 +1,15 @@
+package chap02.section2
+
+fun main() {
+    val number = 10
+    var language = "Korean"
+    val secondNumber: Int = 20
+    language = "English"
+
+    val ex01 = 123L
+
+    println("number: $number")
+    println("language: $language")
+    println("secondNum: $secondNumber")
+    println("$ex01")
+}
