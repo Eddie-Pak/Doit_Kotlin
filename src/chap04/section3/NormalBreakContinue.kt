9ac51f703add4294dff70adc1d7a1efaa4158a2a
Eddie-Pak <foalstn@naver.com>
2024-03-29 03:15:48 +0900

    chap04.section3-End

--- src/chap04/section3/NormalBreakContinue.kt
+++ src/chap04/section3/NormalBreakContinue.kt
@@ -7,4 +7,30 @@ fun main() {
     }
     println()
     println("outside")
+    println()
+    labelBreak()
+}
+
+//fun labelBreak() {
+//    println("labelBreak")
+//    for (i in 1..5) {
+//        second@for (j in 1..5){
+//            if (j == 3) break
+//            println("i:$i, j: $j")
+//        }
+//        println("after for j")
+//    }
+//    println("after for i")
+//}
+
+fun labelBreak() {
+    println("labelBreak")
+    first@for (i in 1..5) {
+        second@for (j in 1..5){
+            if (j == 3) break@first // continue로 바꿔서도 실행해보가
+            println("i:$i, j: $j")
+        }
+        println("after for j")
+    }
+    println("after for i")
 }
--- src/chap04/section3/TryCatch.kt
+++ src/chap04/section3/TryCatch.kt
@@ -13,3 +13,21 @@ fun main() {
         println("finally 블록은 반드시 항상 실행됨")
     }
 }
+
+// 산술 연산에 대한 예외를 따로 특정해서 잡아보는 코드
+//try {
+//    c = a/b
+//} catch (e: ArithmeticException){
+//    println("Exception is handled. ${e.message}")
+//} finally {
+//    println("finally 블록은 반드시 항상 실행됨")
+//}
+//
+// 임시메모리 영역인 스택을 추적할수있는 코드
+//try {
+//    c = a/b
+//} catch (e: Exception){
+//    e.printStackTrace()
+//} finally {
+//    println("finally 블록은 반드시 항상 실행됨")
+//}
