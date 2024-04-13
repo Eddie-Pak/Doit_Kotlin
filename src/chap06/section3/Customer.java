037d2c79f4873c387b4d717fb98681afc5cfb4bf
Eddie-Pak <foalstn@naver.com>
2024-04-13 16:52:52 +0900

    chap06.section3-1

---
+++ src/chap06/section3/Customer.java
@@ -0,0 +1,8 @@
+package chap06.section3;
+
+public class Customer {
+    public static final String LEVEL = "BASIC"; // static 필드
+    public static void login() { // static 필드
+        System.out.println("Login...");
+    }
+}
